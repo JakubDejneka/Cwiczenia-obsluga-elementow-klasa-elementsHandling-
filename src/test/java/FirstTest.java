1da2306da9fbb4520e79603b99bfaeb75362822f
jakubdejneka <j.dejneka@wp.pl>
2020-05-28 23:06:34 +0200

    gotowy szablonik selenium webdriver + maven - java

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class FirstTest {
+    @Test
+    public void demoTest(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.navigate().to("http://www.google.com");
+
+    }
+
+
+
+}
